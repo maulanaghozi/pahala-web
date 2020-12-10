ba0ab1c7a5cbdfb6ac85d193533c82a885a04e9f
Kukka Aiko Farza Lawendatu <maulana.ghozi@qbit.co.id>
2020-12-10 18:09:50 +0700

    finishing ketentuan permohonan bantuan

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import style from "./App.module.scss";
+import { IconRedCross } from "./assets";
 const langkahBantuan = [
   "Register pemohonan bantuan.",
   "Upload materi proposal berbentuk video, jpg di halaman yang telah disediakan.",
   "Pastikan penerima bantuan telah memberikan izin disertai surat pernyataan jika pengguna hanya membantu orang lain.",
   "Informasi yang disajikan pengguna menjadi tanggung jawab sepenuhnya pengguna.",
 ];
+
 function App() {
   return (
     <div className={style.container}>
@@ -19,6 +21,32 @@ function App() {
           </div>
         ))}
         <h3>Permohonan Bantuan yang Tidak Diperkenankan</h3>
+        <div className={style.list}>
+          <IconRedCross width={25} height={25} />
+          <p>
+            Bantuan bersifat <strong>politik</strong>
+          </p>
+        </div>
+        <div className={style.list}>
+          <IconRedCross width={25} height={25} />
+          <p>
+            Bantuan untuk kegiatan <strong>terorisme dan kejahatan</strong> yang
+            dilarang oleh Undang-Undang Pemerintah
+          </p>
+        </div>
+        <div className={style.list}>
+          <IconRedCross width={25} height={25} />
+          <p>
+            Informasi bersifat <strong>fiktif</strong>
+          </p>
+        </div>
+        <div className={style.note}>
+          <p>
+            Pahala Express berhak menutup sepihak pemohon bantuan yang melanggar
+            aturan kami. Baca selengkapnya <span>di sini</span>.
+          </p>
+        </div>
+        <button>Mulai Permohonan Bantuan</button>
       </div>
     </div>
   );
---
+++ src/layout/bottomNavigation/bottomNavigation.js
@@ -0,0 +1,6 @@
+import React from "react";
+import style from "./BottomNavigation.module.scss";
+
+export default function BottomNavigation(props) {
+  return <div></div>;
+}
