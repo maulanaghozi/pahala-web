e44df5cca4d9e955b060fd07eb4a3eecf5bfc23a
Kukka Aiko Farza Lawendatu <maulana.ghozi@qbit.co.id>
2020-12-10 16:37:42 +0700

    import sass to procject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import style from "./App.module.scss";
+const langkahBantuan = [
+  "Register pemohonan bantuan.",
+  "Upload materi proposal berbentuk video, jpg di halaman yang telah disediakan.",
+  "Pastikan penerima bantuan telah memberikan izin disertai surat pernyataan jika pengguna hanya membantu orang lain.",
+  "Informasi yang disajikan pengguna menjadi tanggung jawab sepenuhnya pengguna.",
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.container}>
+      <div className={style.wrapper}>
+        <h2>Permohonan Bantuan</h2>
+        <p>Sebelum membuat permohonan bantuan, baca ini dulu ya: </p>
+        <h3>Langkah Permohonan Bantuan</h3>
+        {langkahBantuan.map((item, idx) => (
+          <div className={style.list} key={idx}>
+            <span>{idx + 1}</span>
+            <p>{item}</p>
+          </div>
+        ))}
+        <h3>Permohonan Bantuan yang Tidak Diperkenankan</h3>
+      </div>
     </div>
   );
 }
