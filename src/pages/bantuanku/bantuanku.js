5013b3d7e73b8bd3c00d5b5d173b59affda24d26
Kukka Aiko Farza Lawendatu <maulana.ghozi@qbit.co.id>
2020-12-21 11:06:53 +0700

    create empty data for bantuanku

--- src/pages/bantuanku/bantuanku.js
+++ src/pages/bantuanku/bantuanku.js
@@ -3,12 +3,13 @@ import TopNavigation from "../../layout/topNavigation/topNavigation";
 import { Route, Switch, Redirect } from "react-router-dom";
 import LongButton from "../../components/longButton/longButton";
 import style from "./bantuanku.module.scss";
+import { IconMembantu, IconPermohonan } from "../../assets";
 
 export default function Bantuanku(props) {
   return (
     <div>
       <TopNavigation>
-        <div>
+        <>
           <Switch>
             <Route path={"/bantuanku/permohonan"} component={Perkembanganku} />
             <Route path={"/bantuanku/membantu"} component={Terkini} />
@@ -17,7 +18,7 @@ export default function Bantuanku(props) {
               render={() => <Redirect to={"/bantuanku/permohonan"} />}
             />
           </Switch>
-        </div>
+        </>
       </TopNavigation>
     </div>
   );
@@ -26,6 +27,11 @@ export default function Bantuanku(props) {
 const Perkembanganku = () => {
   return (
     <>
+      <IconPermohonan className={style.icon} />
+      <span className={style.primaryText}>Belum ada permohonan</span>
+      <span className={style.secondaryText}>
+        Supaya dapat update dari permohonan bantuan, mulai bantu yuk!
+      </span>
       <LongButton className={style.button} text={"Tambah Permohonan"} />
     </>
   );
@@ -34,7 +40,13 @@ const Perkembanganku = () => {
 const Terkini = () => {
   return (
     <>
-      <LongButton text={"Bantu Sekarang"} />
+      <IconMembantu className={style.icon} />
+      <span className={style.primaryText}>Belum ada membantu</span>
+      <span className={style.secondaryText}>
+        Bantuan anda untuk permohonan orang lain akan tercatat dan muncul
+        disini, yuk mulai bantu!
+      </span>
+      <LongButton className={style.button} text={"Bantu Sekarang"} />
     </>
   );
 };
